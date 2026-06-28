8ec76275969b1abd2f01e4e7055f8a17d65a12b7
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-28 23:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if(s=="") return "";
+	let n=s.trim().length;
+	let j=0;
+	for(let i=0;i<n;i++){
+		if(s[i]!=" ") j++;
+		else break;
+	}
+	return s.slice(0,j);
 }
 
 // Do not change the code below
