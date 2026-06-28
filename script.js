9fa1e3b95a0219679d63275047b1077d6b497361
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-28 23:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function firstWord(s) {
 
 // Do not change the code below
 
-// const s = prompt("Enter String:");
-// alert(firstWord(s));
+const s = prompt("Enter String:");
+alert(firstWord(s));
 
 
